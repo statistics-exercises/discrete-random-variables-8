0c809c3b0fee8e4da9307c3e7597d09b5d52bd13
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-26 18:51:39 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# You may wish to write some code here
+
+def geometric(p) :
+  # Your code for generating a geometric random variable goes here
+  
+# Your code for generating the list of geometric random variables goes here
+prob = 0.5
+  
+# This will plot a graph showing your Bernoulli random variables.
+plt.plot( indices, random_variables, ".") 
+plt.savefig("geometric_rvs.png")
