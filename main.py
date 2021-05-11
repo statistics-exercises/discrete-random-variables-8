c426a231696932965523f5dc47c608ec6a003151
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-11 15:11:57 +0100

    Added solution

--- main.py
+++ main.py
@@ -5,7 +5,19 @@ import numpy as np
 
 def geometric(p) :
   # Your code for generating a geometric random variable goes here
-  
+  nt = 1
+  while np.random.uniform(0,1)>p : nt = nt + 1
+  return nt 
+ 
 # Your code for generating the list of geometric random variables goes here
 prob = 0.5
+xvals, yvals = np.zeros(100), np.zeros(100)
+for i in range(100) : 
+    xvals[i] = i+1 
+    yvals[i] = geometric(prob)
+
+plt.plot( xvals, yvals, 'ko' )
+plt.xlabel("Index")
+plt.ylabel("random variable")
+plt.savefig("geometric.png")
   
